82b61fc538901822680bb2b251b82fa42d3283f4
Jonas Bonér <jonasremove@jonasboner.com>
2010-05-03 21:56:52 +0200

    fixed missing repository

--- pipeline/akka/project/build/AkkaBench.scala
+++ pipeline/akka/project/build/AkkaBench.scala
@@ -10,6 +10,11 @@ class AkkaBench(info: ProjectInfo) extends DefaultProject(info) {
   val codehaus_snapshots = "Codehaus Snapshots" at "http://snapshots.repository.codehaus.org"
   val jboss = "jBoss" at "http://repository.jboss.org/maven2"
   val m2 = "m2" at "http://download.java.net/maven/2"
+  val guiceyfruit = "GuiceyFruit" at "http://guiceyfruit.googlecode.com/svn/repo/releases/"
+  val google = "Google" at "http://google-maven-repository.googlecode.com/svn/repository"
+  val java_net = "java.net" at "http://download.java.net/maven/2"
+  val scala_tools_snapshots = "scala-tools snapshots" at "http://scala-tools.org/repo-snapshots"
+  val scala_tools_releases = "scala-tools releases" at "http://scala-tools.org/repo-releases"
 
   // deps
   val akka_core = "se.scalablesolutions.akka" % "akka-core_2.8.0.Beta1" % "0.8.1" % "compile"
